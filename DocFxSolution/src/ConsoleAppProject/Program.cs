abb01b3319c930a8c8110d1fae026ab55e75a874
yoshi4b2 <81914712+yoshi4b2@users.noreply.github.com>
2025-12-03 22:26:53 +0900

    ビルドをコミット

---
+++ DocFxSolution/src/ConsoleAppProject/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleAppProject;
+
+/// <summary>
+/// アプリケーションのエントリーポイントを提供するクラス
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// アプリケーションのメインメソッド
+    /// </summary>
+    /// <param name="args">コマンドライン引数の配列</param>
+    public static void Main(string[] args)
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+
+        if (args.Length > 0)
+        {
+            Console.WriteLine("コマンドライン引数:");
+            foreach (var arg in args)
+            {
+                Console.WriteLine($"- {arg}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("引数は指定されていません。");
+        }
+    }
+}
